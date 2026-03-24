27dded032d8e163e1c1862d9d596f63cc487c0eb
Derek Stritzel <dstritzel@user.noreply.github.com>
2026-03-24 07:30:32 -0400

    Homebrew additions and nvim tagging

--- hooks/pre-up/75-tag-nvim.sh
+++ hooks/pre-up/75-tag-nvim.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
 
-#TODO: See if we can make this look cleaner.
-if ! grep -q -w "nvim" <<< "$TAGS"; then
-  echo "nvim tag not found: skipped."
-  exit 0
+if [[ ! -d /home/linuxbrew/.linuxbrew && ! -d /opt/homebrew ]]; then
+  echo "Homebrew is not installed. It is required for the latest neovim version."
+  exit 1
+else
+  if [[ "$OS_TYPE" == "linux" && ! "$PATH" =~ "/home/linuxbrew/.linuxbrew/bin" ]]; then
+    echo "Homebrew is not in the PATH. Adding it to the PATH."
+    export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+  elif [[ "$OS_TYPE" == "mac" && ! "$PATH" =~ "/opt/homebrew/bin" ]]; then
+    echo "Homebrew is not in the PATH. Adding it to the PATH."
+    export PATH="/opt/homebrew/bin:$PATH"
+  fi
 fi
 
-if [[ -d "${HOME}/config/nvim" ]]; then
-  echo "nvim LazyVim already cloned"
-  # TODO: Work on more checks and do a git update
-else
+if ! brew list "neovim"; then
+  echo "neovim is not installed. Installing..."
+  brew install neovim
+  #TODO: add check if we need to update neovim.
+fi
+
+if [[ ! -d "${HOME}/config/nvim" ]]; then
+  echo "nvim LazyVim not cloned. Cloning..."
   git clone https://github.com/LazyVim/starter.git "${HOME}/config/nvim"
+else
+  echo "nvim LazyVim already cloned. Updating..."
+  git -C "${HOME}/config/nvim" pull
 fi
